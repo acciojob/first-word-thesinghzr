10d7c9626d3e12b1400d7828dd59522e60767a13
Suraj Singh <yousurajsingh@gmail.com>
2024-12-29 20:54:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function firstWord(str) {
-    if (!str) return ""; // Return an empty string if the input is empty
+    
     const firstSpaceIndex = str.indexOf(" "); // Find the index of the first space
     return firstSpaceIndex === -1 ? str : str.slice(0, firstSpaceIndex); // Return the entire string or up to the first space
 }
